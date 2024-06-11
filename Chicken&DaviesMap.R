750cec73a2288430ce4bf4bd2c6987812342fe33
NaoSnkn <sananikone.naomi@gmail.com>
2024-06-11 15:14:14 +1000

    Chicken&DaviesMap.R

--- Chicken&DaviesMap.R
+++ Chicken&DaviesMap.R
@@ -1,4 +1,6 @@
-# for maps
+### Code inspired by Nielsen et al. (2022) ###
+
+Required packages
 install.packages("remotes")
 remotes::install_github("https://github.com/open-AIMS/gisaimsr")
 library(raster) # for map
@@ -7,29 +9,26 @@ library(sf)
 library(dataaimsr)
 library(gisaimsr)
 library(ggrepel)
-library(rgeos)
 library(rnaturalearth)
 library("readxl")
 library(ggplot2)
 library(remotes)
-install.packages("rnaturalearthdata")
 library(rnaturalearthdata)
 
-
+# Call data
 coordsmap <- read_excel("D:/AIMS/Master Thesis/Data/Coordinates.xlsx")
-
+# Reefs coordinates
 Coord <- data.frame(Coord = ("Coord"), lat = -18.7, lon = 147.65)
 
-gbr_feat
-FNGBR = ggplot() +
-  geom_sf(data = gbr_feat, lwd = 0) +
-  annotation_scale(location = "bl", width_hint = 0.5) +
+Map = ggplot() +
+  geom_sf(data = gbr_feat, lwd = 0) + # Uses australia and reefs data
+  annotation_scale(location = "bl", width_hint = 0.5) + # Scale of the map
   annotation_north_arrow(location = "bl", which_north = "true",
                          pad_x = unit(0.25, "in"), pad_y = unit(0.4, "in"),
-                         style = north_arrow_fancy_orienteering) +
+                         style = north_arrow_fancy_orienteering) + # Adds North arrow
   coord_sf(xlim = c(146, 149), ylim = c(-19.5, -18)) +
   geom_point(data = coordsmap, mapping = aes(x = lon, y = lat), size=2.8) +
-  geom_text_repel(data = coordsmap, 
+  geom_text_repel(data = coordsmap, # Labels reefs
                    mapping = aes(x = lon, y = lat,
                                  label = Reef),
                    vjust=4,
@@ -40,32 +39,27 @@ FNGBR = ggplot() +
         axis.text = element_text(family="Imprima"),
         legend.title = element_text(family="Product Sans"),
         legend.text = element_text(family="Product Sans"),
-        panel.background = element_rect(fill = "#E6F5F7"),
+        panel.background = element_rect(fill = "#E6F5F7"), # Water in blue
         panel.border = element_rect(fill = "transparent"),
         panel.grid = element_blank())+
    labs(x = "Longitude",
        y = "Latitude",
        title = "",
        subtitle = "")
-FNGBR
+Map
 
-?geom_sf
-
-oz <- ne_countries(country = "Australia", scale='small',returnclass = 'sf')
-
-inset = ggplot() +
-  geom_sf(data = oz, fill = "grey80", col = "transparent") +
+# Add Australia miniature
+Australia <- ne_countries(country = "Australia", scale='small',returnclass = 'sf')
+# Add circle pointing desired area on Australian's map
+Dot = ggplot() +
+  geom_sf(data = Australia, fill = "grey80", col = "transparent") +
   geom_point(data = Coord, mapping = aes(x = lon, y = lat), size=7, colour="grey20")+
   theme_void() +
   theme(legend.position = "none") 
-inset
+Dot
 
- #Australia
-annotated_map = FNGBR + 
-  annotation_custom(ggplotGrob(inset), xmin = 148, xmax = 149, ymin = -18.8, ymax = -18)
-  
-g1 <- annotated_map
-g1
+# Add Australia and dot to the map
+FinalMap = Map + 
+  annotation_custom(ggplotGrob(Dot), xmin = 148, xmax = 149, ymin = -18.8, ymax = -18)
 
-
-#ggsave("HeatStressMethodsMap.jpeg")
+FinalMap
