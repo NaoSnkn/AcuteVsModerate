c1945a77e3bf84c3906aa3e53051bd16d68d6615
NaoSnkn <sananikone.naomi@gmail.com>
2024-06-11 15:24:51 +1000

    Chicken&DaviesTemperatures.R

--- Chicken&DaviesTemperatureProfiles.R
+++ Chicken&DaviesTemperatureProfiles.R
@@ -1,4 +1,4 @@
-# Libraries
+# Required Packages: 
 library(ggplot2)
 library(dplyr)
 library(hrbrthemes)
@@ -6,15 +6,88 @@ library(lubridate)
 library("readxl")
 library(ggpubr)
 
+# Call data: 
 Temp <- read_excel("D:/AIMS/Master Thesis/Data/Temperature/ChickenDaviesSiteTemp.xlsx")
-str(Temp) # Show you data properties
 Temp$Temperature <- as.numeric(Temp$Temperature) # Make sure that Temperature is numeric
 Temp$Time <- ymd_hms(Temp$Time, tz = "Australia/Brisbane") #Turn data from character to date
 
-qqnorm(Temp$Temperature)
-kruskal.test(Temp$Temperature~Temp$Site)
+## DATA ANALYSIS ##
 
-# Most basic bubble plot
+# Statistics on data set
+qqnorm(Temp$Temperature) # Normality assessment (not normal)
+kruskal.test(Temp$Temperature~Temp$Site) # Difference between sites
+
+# Data aggregation
+    # Per day
+Temp_DayAggregated <- Temp %>%
+  mutate(Date = floor_date(Time, unit = "day")) %>%
+  group_by(Site, Date) %>%
+  summarise(
+    MeanTemperature = mean(Temperature),
+    MaxTemperature = max(Temperature),
+    MinTemperature = min(Temperature),
+    DiurnalVariation = max(Temperature)-min(Temperature)
+  )
+Temp_DayAggregated <- Temp_DayAggregated %>%
+  pivot_longer(cols = c(MinTemperature, MeanTemperature, MaxTemperature, DiurnalVariation), 
+               names_to = "Type", 
+               values_to = "Temperature") %>%
+  mutate(Type = case_when(
+    Type == "MaxTemperature" ~ "Daily Maximum",
+    Type == "MinTemperature" ~ "Daily Minimum",
+    Type == "MeanTemperature" ~ "Mean daily",
+    Type == "DiurnalVariation" ~ "Diurnal variation"
+  ))
+Temp_DayAggregated 
+
+    # Per month
+Temp_MonthAggregated <- Temp_DayAggregated |> 
+  mutate(Month = floor_date(Date, unit = "month"))  |>
+  group_by(Site, Month) |> 
+  summarise(MeanDiurnalVariation = mean(DiurnalVariation))
+Temp_MonthAggregated
+
+    # Wilcoxon tests #
+
+# Compare Mean Diurnal Variation per Month
+Temp_MonthAggregated <- Temp_MonthAggregated |> 
+  pivot_wider(names_from = Site, values_from = MeanDiurnalVariation) 
+  wilcox.test(Temp_MonthAggregated$`Chicken Reef`, Temp_MonthAggregated$`Davies Reef`)
+
+# Compare Daily Minimum Temperature
+Temp_MinDay <- subset(Temp_DayAggregated, Type=="Daily Minimum")
+Temp_MinDay <- Temp_MinDay %>%
+  pivot_wider(names_from = Site, values_from = Temperature)
+wilcox.test(Temp_MinDay$`Chicken Reef`, Temp_MinDay$`Davies Reef`)
+
+# Compare Daily Maximum Temperature
+Temp_MaxDay <- subset(Temp_DayAggregated, Type=="Daily Maximum")
+Temp_MaxDay <- Temp_MaxDay %>%
+  pivot_wider(names_from = Site, values_from = Temperature)
+wilcox.test(Temp_MinDay$`Chicken Reef`, Temp_MinDay$`Davies Reef`)
+
+# Compare Daily Temperature Mean
+Temp_MeanDay <- subset(Temp_DayAggregated, Type=="Mean daily")
+Temp_MeanDay <- Temp_MeanDay |> 
+  pivot_wider(names_from = Site, values_from = Temperature)
+  wilcox.test(Temp_MeanDay$`Chicken Reef`, Temp_MeanDay$`Davies Reef`)
+
+# Compare Diurnal temperature variation
+Temp_DayVar <- subset(Temp_DayAggregated, Type=="Diurnal variation")
+Temp_DayVar <- Temp_DayVar |> 
+  pivot_wider(names_from = Site, values_from = Temperature)
+  wilcox.test(Temp_DayVar$`Chicken Reef`, Temp_DayVar$`Davies Reef`)
+
+# Other stats
+summary(Temp_DayVar$`Chicken Reef`) # Get mean, min and max
+sd(Temp_DayVar$`Chicken Reef`) / sqrt(length(Temp_DayVar$`Chicken Reef`)) #Standard error
+summary(Temp_DayVar$`Davies Reef`)
+sd(Temp_DayVar$`Davies Reef`) / sqrt(length(Temp_DayVar$`Davies Reef`))
+
+    ## DATA VIZUALISATION ##
+
+  ## Temperature profile throughout the year ##
+
 p <- ggplot(Temp, aes(x=Time, y=Temperature, color=Site)) +
   geom_line(linewidth=0.5) +
   xlab("Date") +
@@ -28,87 +101,11 @@ p <- ggplot(Temp, aes(x=Time, y=Temperature, color=Site)) +
         legend.text = element_text(family="Product Sans Light")) +
   ylim(22.5,31.5)
 p
-?scale_x_datetime()
-# Change axis to enflish
-# Male axis start
-# Change background grid
-# Customize fonts and size
 
-original_locale <- Sys.getlocale("LC_TIME")
-
-# Changer la locale pour l'anglais
-Sys.setlocale("LC_TIME", "en_US.UTF-8")
-Sys.setlocale("LC_TIME", original_locale)
-
-
-# Agrégation des données par jour et par site
-Temp_DayAggregated <- Temp %>%
-  mutate(Date = floor_date(Time, unit = "day")) %>%
-  group_by(Site, Date) %>%
-  summarise(
-    MeanTemperature = mean(Temperature),
-    MaxTemperature = max(Temperature),
-    MinTemperature = min(Temperature),
-    DiurnalVariation = max(Temperature)-min(Temperature)
-  )
-
-Temp_MonthAggregated <- Temp_DayAggregated |> 
-  mutate(Month = floor_date(Date, unit = "month"))  |>
-  group_by(Site, Month) |> 
-  summarise(MeanDiurnalVariation = mean(DiurnalVariation))
-
-Temp_MonthAggregated
-
-Temp_MonthAggregated <- Temp_MonthAggregated |> 
-  pivot_wider(names_from = Site, values_from = MeanDiurnalVariation) 
-  wilcox.test(Temp_MonthAggregated$`Chicken Reef`, Temp_MonthAggregated$`Davies Reef`)
-
-# Affichage des résultats
-
-Temp_DayAggregated <- Temp_DayAggregated %>%
-  pivot_longer(cols = c(MinTemperature, MeanTemperature, MaxTemperature, DiurnalVariation), 
-               names_to = "Type", 
-               values_to = "Temperature") %>%
-  mutate(Type = case_when(
-    Type == "MaxTemperature" ~ "Daily Maximum",
-    Type == "MinTemperature" ~ "Daily Minimum",
-    Type == "MeanTemperature" ~ "Mean daily",
-    Type == "DiurnalVariation" ~ "Diurnal variation"
-  ))
-
-shapiro.test(Temp_DayAggregated$Temperature)
-
-Temp_MinDay <- subset(Temp_DayAggregated, Type=="Daily Minimum")
-Temp_MinDay <- Temp_MinDay %>%
-  pivot_wider(names_from = Site, values_from = Temperature)
-wilcox.test(Temp_MinDay$`Chicken Reef`, Temp_MinDay$`Davies Reef`)
-
-Temp_MaxDay <- subset(Temp_DayAggregated, Type=="Daily Maximum")
-Temp_MaxDay <- Temp_MaxDay %>%
-  pivot_wider(names_from = Site, values_from = Temperature)
-wilcox.test(Temp_MinDay$`Chicken Reef`, Temp_MinDay$`Davies Reef`)
-
-Temp_MeanDay <- subset(Temp_DayAggregated, Type=="Mean daily")
-Temp_MeanDay <- Temp_MeanDay |> 
-  pivot_wider(names_from = Site, values_from = Temperature)
-  wilcox.test(Temp_MeanDay$`Chicken Reef`, Temp_MeanDay$`Davies Reef`)
-  
-Temp_DayVar <- subset(Temp_DayAggregated, Type=="Diurnal variation")
-Temp_DayVar <- Temp_DayVar |> 
-  pivot_wider(names_from = Site, values_from = Temperature)
-  wilcox.test(Temp_DayVar$`Chicken Reef`, Temp_DayVar$`Davies Reef`)
-  
-summary(Temp_DayVar$`Chicken Reef`) # Get mean, min and max
-sd(Temp_DayVar$`Chicken Reef`) / sqrt(length(Temp_DayVar$`Chicken Reef`)) #Standard error
-summary(Temp_DayVar$`Davies Reef`)
-sd(Temp_DayVar$`Davies Reef`) / sqrt(length(Temp_DayVar$`Davies Reef`))
-
-
-# Calculate standard error
-
-
-
+# Arrange
 Temp_DayAggregated$Type <- factor(Temp_DayAggregated$Type, levels = c("Daily Minimum","Daily Maximum","Mean daily","Diurnal variation"))
+
+    ## Boxplots ##
 
 m <- ggplot(data=Temp_DayAggregated, aes(x=Site, y=Temperature, fill=Site)) +
   geom_boxplot()+
@@ -124,13 +121,7 @@ m <- ggplot(data=Temp_DayAggregated, aes(x=Site, y=Temperature, fill=Site)) +
         legend.text = element_text(family="Product Sans Light"))
 m
 
-
-#Make grid
-#Customize background
-#Customize fonts
-
 ggarrange(p, m, ncol = 1,
           labels = c("A","B"),
           nrow = 2, 
           heights = c(3,2))
-?ggarrange()
